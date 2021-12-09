ff4507e44c7d0db9a1e200f6ff3b581ae47ac250
AzsenSeries <thanapatnes@gmail.com>
2021-12-09 16:32:50 +0700

    9

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
-import Navbar from "../components/Navbar";
-import Yourmenu from "../components/yourmenu";
-import Footer from "../components/footer"
-import Testrandom from "./testrandom";
+import Login from "./Login";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <Yourmenu />
-      {/* <Testrandom/> */}
-      <div className="">
-        <Footer/>
-      </div>
+      <Login/>
     </div>
   );
 }
--- pages/login.js
+++ pages/login.js
@@ -1,94 +1,67 @@
-import React from 'react'
-import { useState } from 'react';
-import Navbar from '../components/Navbar';
-import Swal from 'sweetalert2';
-import { useRouter } from "next/router";
-import Footer from '../components/footer';
+import { LockClosedIcon } from '@heroicons/react/solid'
 
-
-export default function Login() {
-    const router = useRouter();
-
-    const [username, setUsername] = useState([]);
-    const [password, setPassword] = useState([]);
-  
-    const handleAddProduct = async (e) => {
-      e.preventDefault();
-      if (username === 'admin' && password === '0000') {
-        window.sessionStorage.setItem("login", "true");
-        router.push("/addfood");
-    }else 
-      Swal.fire({
-        title: "Username หรือ Password ไม่ถูกต้องค่ะ",
-        width: 600,
-        padding: "3em",
-        // background: '#fff url(/images/trees.png)',
-        backdrop: `
-          rgba(0,0,123,0.4)
-          url("/images/nyan-cat.gif")
-          left top
-          no-repeat
-        `,
-      });
-    };
+export default function Example() {
     return (
         <>
-        <div>
-        <Navbar />
-        <div className="container mx-auto max-w-lg py-10">
-            <div className="px-4 flex-auto">
+            <div className="container mx-auto max-w-lg py-10">
+                <div className="px-4 flex-auto">
+                    <div className="py-5 block grid">
+                        <div className="cursor-pointer mb-3 border-2 border-gray-300 rounded-lg px-3 bg-white">
+                                <div className="max-w-md w-full space-y-8">
+                                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
+                                    <form className="w-full max-w-lg">
+                                        <input className="appearance-none block w-full bg-gray-50 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"
+                                            type="text"
+                                            name="user-name"
+                                            placeholder="Username"
+                                        />
+                                        <input className="appearance-none block w-full bg-gray-50 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text"
+                                            type="text"
+                                            name="user-name"
+                                            placeholder="Password"
+                                        />
+                                        <div className="flex items-center justify-between">
+                                            <div className="flex items-center">
+                                                <input
+                                                    id="remember-me"
+                                                    name="remember-me"
+                                                    type="checkbox"
+                                                    className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                                                />
+                                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                                                    Remember me
+                                                </label>
+                                            </div>
 
-                <div className="py-5 block grid">
-                    <div className="cursor-pointer mb-3 border-2 border-gray-300 rounded-lg px-3 bg-white">
-                        <form className="w-full max-w-lg">
-                            <div className="py-5 text-center">
-                                <h1 className="text-2xl bg-shadow block uppercase tracking-wide font-bold">Signin admin</h1>
+                                            <div className="text-sm py-5">
+                                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                                    Forgot your password?
+                                                </a>
+                                            </div>
+                                        </div>
+
+                                        <div>
+                                            <button
+                                                type="submit"
+                                                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                            >
+                                                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                                                    <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                                                </span>
+                                                Sign in
+                                            </button>
+                                        </div>
+                                        <div className="text-sm text-center py-5">
+                                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                            Forgot your password?
+                                        </a>
+                                    </div>
+                                    </form>
+                                </div>
                             </div>
-                            <div className="w-full px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs mb-2" htmlFor="grid-first-name">
-                                    Username
-                                </label>
-                                <input className="appearance-none block w-full bg-gray-50 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" 
-                                 type="text"
-                                 name="user-name"
-                                 placeholder="Username"
-                                 value={username}
-                                 onChange={(e) => {
-                                   setUsername(e.target.value);
-                                 }}
-                                />
-                            </div>
-                            <div className="w-full px-3">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs mb-2" htmlFor="grid-first-name">
-                                    Password
-                                </label>
-                                <input className="appearance-none block w-full bg-gray-50 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" 
-                                type="password"
-                                name="user-password"
-                                placeholder="Password"
-                                value={password}
-                                onChange={(e) => {
-                                  setPassword(e.target.value);
-                                }}/>
-                            </div>
-                            <div className="py-5 text-center my-4">
-                                <button
-                                    className="bg-yellow-400 uppercase tracking-wide hover:shadow-lg text-white font-bold py-2 px-4 rounded"
-                                    type="submit"
-                                    value="submit"
-                                    name="submit"
-                                    onClick={handleAddProduct}
-                                >
-                                    signin
-                                </button>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
-        </div>
-        <Footer/>
-    </div>
-    </>
+        </>
     )
 }
