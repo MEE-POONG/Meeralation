c0ef78dec83f5ce28984c41805424e8a3a5e576a
AzsenSeries <thanapatnes@gmail.com>
2022-01-11 17:52:58 +0700

    .

--- pages/feel.js
+++ pages/feel.js
@@ -1,40 +1,44 @@
 import React from "react";
 import { AiOutlinePlus, AiOutlinePicture } from "react-icons/ai";
 import Status from "../components/Status";
+import Sidebar from "../components/Sidebar";
 
 export default function Feel() {
   return (
-    <div className="mx-auto w-full container">
-      <div className="grid grid-cols-4  text-white ">
-        <button className=" box-border p-4 m-2 rounded-lg bg-gray-600  ">
-          <div className=" inline-block">
-            {" "}
-            <AiOutlinePlus size={30} />
-          </div>
-        </button>
-        <button className="box-border h-32 p-4 m-2 rounded-lg bg-gray-600">
-          <div className=" inline-block">
-            {" "}
-            <AiOutlinePicture size={30} />
-            Rick White
-          </div>
-        </button>
-        <button className="box-border h-32  p-4 m-2 rounded-lg bg-gray-600">
-          <div className=" inline-block">
-            {" "}
-            <AiOutlinePicture size={30} />
-            Rick White
-          </div>
-        </button>
-        <button className="box-border h-32  p-4 m-2 rounded-lg bg-gray-600">
-          <div className=" inline-block">
-            {" "}
-            <AiOutlinePicture size={30} />
-            Rick White
-          </div>
-        </button>
-      </div>
-      <Status />
-    </div>
+    <><Sidebar />
+      <div className="mx-auto w-full container mt-5">
+        <div className="grid grid-cols-4  text-white ">
+          <button className=" box-border p-4 m-2 rounded-lg bg-gray-600  ">
+            <div className=" inline-block">
+              {" "}
+              <AiOutlinePlus size={30} />
+            </div>
+          </button>
+          <button className="box-border h-32 p-4 m-2 rounded-lg bg-gray-600">
+            <div className=" inline-block">
+              {" "}
+              <AiOutlinePicture size={30} />
+              Rick White
+            </div>
+          </button>
+          <button className="box-border h-32  p-4 m-2 rounded-lg bg-gray-600">
+            <div className=" inline-block">
+              {" "}
+              <AiOutlinePicture size={30} />
+              Rick White
+            </div>
+          </button>
+          <button className="box-border h-32  p-4 m-2 rounded-lg bg-gray-600">
+            <div className=" inline-block">
+              {" "}
+              <AiOutlinePicture size={30} />
+              Rick White
+            </div>
+          </button>
+        </div>
+        <div className="px-5">
+          <Status />
+        </div>
+      </div></>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Sidebar from "../components/Sidebar";
+
 import Login from "./Login";
 // import Register from "./Register";
 
 export default function Home() {
   return (
     <div>
-      <Sidebar/>
       <Login/>
     </div>
   );
